b8aeef875574cb839f8b4f2ea8a20eff03b75c5f
Ubuntu <ubuntu@ip-172-31-33-231.ec2.internal>
2020-11-18 22:21:10 +0000

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by Jonathan Pasco-Arnone
+# Created on November 2020
+# This program prints "Hello, World!"
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
